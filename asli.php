8259d9a40624f2cf57205e0ae7de09af0a58e398
roy354 <44160680+roy354@users.noreply.github.com>
2019-08-26 22:03:06 +0700

    Add files via upload

--- asli.php
+++ asli.php
@@ -2,24 +2,24 @@
 require 'class.php';
 $user = $argv[1];
 $pass = $argv[2];
-
+$waktu_1_hari = 86400;
 // Loop Endless
 while (1) {
-	$tanggal_sekarang = date("d");
+	$jam_awal = round(microtime(1));
 	$baru = new social();
 	$baru->login($user, $pass);
 	$get = $baru->get_primary(); // dapat id postingan baru
 	for ($i = 0; $i < 1500; $i++) {
 		$baru->react($get);
 		//sleep(1);
+		$rand = rand(0, 10);
+		sleep($rand); // Supaya Server askdaraz gak down
 		$id = $baru->cari($get);
 		$get = $id;
 	}
 	$baru->logout();
-	while (1) {
-		if ($tanggal_sekarang != date("d")) {
-			break;
-		}
-	}
+	$jam_akhir = round(microtime(1))-$jam_awal;
+	$sleep = $waktu_1_hari-$jam_akhir;
+	sleep($sleep);
 }
 ?>
